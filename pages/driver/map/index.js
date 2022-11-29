be1278155e300ea12be1f92c1379686086f3ca56
Athicha L. (mac) <nongtonkaew@gmail.com>
2022-11-29 23:22:33 +0700

    Added Driver Maps

--- pages/driver/map/index.js
+++ pages/driver/map/index.js
@@ -2,8 +2,13 @@ import DriverNavBar from "../../../components/DriverNavBar";
 
 export default function DriverMap() {
     return (
-        <div>
-            <DriverNavBar/>
+        <div className="w-screen h-screen bg-primaryBase">
+            <div className="w-full h-full">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d61997.53670482409!2d100.49550318505099!3d13.788152872827336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x30e29ecde3aee521%3A0x9f43939a2caf2963!2sSiam%20Paragon%2C%20Rama%20I%20Road%2C%20Pathum%20Wan%2C%20Bangkok!3m2!1d13.746241099999999!2d100.5347402!4m5!1s0x30e29b73a5a2880d%3A0xda92a156e9f2b465!2sKmutnb%2C%201518%20Phibun%20Songkhram%20Rd%2C%20Wong%20Sawang%2C%20Bang%20Sue%2C%20Bangkok%2010800!3m2!1d13.8191481!2d100.5142331!5e0!3m2!1sen!2sth!4v1669738845598!5m2!1sen!2sth"
+                    className="w-full h-full" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <DriverNavBar className='absolute w-full bottom-0' />
         </div>
     );
 }
