c933ef1dbebcd42adfafaa932ac6c6a1ae38950b
Athicha L. (mac) <nongtonkaew@gmail.com>
2022-11-30 09:43:17 +0700

    Make Button Clickable

--- pages/driver/map/index.js
+++ pages/driver/map/index.js
@@ -17,7 +17,7 @@ export default function DriverMap() {
                     src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d61997.53670482409!2d100.49550318505099!3d13.788152872827336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x30e29ecde3aee521%3A0x9f43939a2caf2963!2sSiam%20Paragon%2C%20Rama%20I%20Road%2C%20Pathum%20Wan%2C%20Bangkok!3m2!1d13.746241099999999!2d100.5347402!4m5!1s0x30e29b73a5a2880d%3A0xda92a156e9f2b465!2sKmutnb%2C%201518%20Phibun%20Songkhram%20Rd%2C%20Wong%20Sawang%2C%20Bang%20Sue%2C%20Bangkok%2010800!3m2!1d13.8191481!2d100.5142331!5e0!3m2!1sen!2sth!4v1669738845598!5m2!1sen!2sth"
                     className="w-full h-full" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <div className="z-50 fixed w-full h-full">
+            <div className="z-50 fixed top-0 left-0 w-full h-full">
                 <div className="absolute bg-red-500 rounded-full right-8 top-8" onClick={() => toggleMenu()}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="white">
                         <path d="M22.5 38V25.5H10v-3h12.5V10h3v12.5H38v3H25.5V38Z" />
