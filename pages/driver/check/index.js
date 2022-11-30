34345ae3fb8e927b2f1128b9300e076b62c1b49d
Athicha L. (mac) <nongtonkaew@gmail.com>
2022-11-30 10:26:41 +0700

    Smth

---
+++ pages/driver/check/index.js
@@ -0,0 +1,22 @@
+function CheckListCard(props) {
+    return (<div className="w-[95%] h-[15%] bg-white text-black m-auto rounded-lg">
+
+        <p className="text-primaryMain text-2xl">{props.name}</p>
+
+    </div>);
+}
+
+const checkList = [
+    {
+        name: '1. เข้างาน / 5. เลิกงาน',
+        svg: (<div></div>)
+    }
+];
+export default function DriverCheck() {
+    return (<div className="w-screen h-screen bg-primaryBase">
+        <p className="text-center text-3xl pt-4 text-primaryMain">ตรวจสภาพรถ</p>
+        {checkList.map((e, idx) => {
+            return <CheckListCard key={idx} name={e.name} icon/>
+        })}
+    </div>);
+}
